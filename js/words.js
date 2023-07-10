15bbdb2f7942e9d2c21a72560544cf861bee5243
Yusuf Semih Sancar <yusufsemihsancar08@gmail.com>
2023-07-10 13:37:59 +0300

    Word-Scramble-Game

---
+++ js/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "jojo",
+        hint: "en iyi anime"
+    },
+    {
+        word: "toplantı",
+        hint: "İnsanların bir araya geldiği olay"
+    },
+    {
+        word: "sayı",
+        hint: "Saymak için kullanılan matematik sembolü"
+    },
+    {
+        word: "değişme",
+        hint: "ticaret eylemi"
+    },
+    {
+        word: "tuval",
+        hint: "Yağlı boya için kumaş parçası"
+    },
+    {
+        word: "bahçe",
+        hint: "Çiçek ve bitki dikmek için alan"
+    },
+    {
+        word: "konum",
+        hint: "Birinin veya bir şeyin yeri"
+    },
+    {
+        word: "tüy",
+        hint: "Tüyleri kuşun dış örtüsü gibi"
+    },
+    {
+        word: "konfor",
+        hint: "Hoş bir rahatlama hissi"
+    },
+    {
+        word: "dil",
+        hint: "Ağzın kaslı organı"
+    },
+    {
+        word: "genleşme",
+        hint: "Artma veya büyüme süreci"
+    },
+    {
+        word: "ülke",
+        hint: "Politik olarak tanımlanmış bir bölge"
+    },
+    {
+        word: "grup",
+        hint: "Bir dizi nesne veya kişi"
+    },
+    {
+        word: "tatmak",
+        hint: "Dilin lezzeti algılama yeteneği"
+    },
+    {
+        word: "mağaza",
+        hint: "Malların ticaretinin yapıldığı büyük dükkan"
+    },
+    {
+        word: "alan",
+        hint: "Tarım faaliyetleri için arazi alanı"
+    },
+    {
+        word: "arkadaş",
+        hint: "Aile üyesi dışındaki kişi"
+    },
+    {
+        word: "cep",
+        hint: "Küçük eşyaları taşımak için bir çanta"
+    },
+    {
+        word: "iğne",
+        hint: "İnce ve keskin bir metal pim"
+    },
+    {
+        word: "uzman",
+        hint: "Kapsamlı bilgiye sahip kişi"
+    },
+    {
+        word: "ifade",
+        hint: "Bir şeyin beyanı"
+    },
+    {
+        word: "saniye",
+        hint: "Dakikanın altmışta biri"
+    },
+    {
+        word: "kütüphane",
+        hint: "Kitap koleksiyonu içeren yer"
+    },
+]
